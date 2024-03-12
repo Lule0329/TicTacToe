a7e280e6eb3c807c7072dc936294e101a4a0b4a8
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-03-12 10:54:26 +0100

    :/

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -42,19 +42,26 @@ namespace TicTacToe
             for (int i = 0; i < 3; i++)
             {
                 if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ')
+                {
                     return board[i, 0]; // Det finns en vinnare horisontellt
+                }
 
                 if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ')
+                { 
                     return board[0, i]; // Det finns en vinnare vertikalt
+                }
             }
 
             // Kolla diagonalt
             if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ')
+            {
                 return board[0, 0]; // Det finns en vinnare diagonalt (från vänster upp till höger ner)
+            }
 
             if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' ')
+            {
                 return board[0, 2]; // Det finns en vinnare diagonalt (från höger upp till vänster ner)
-
+            }
             return ' '; // Ingen vinnare ännu
         }
 
