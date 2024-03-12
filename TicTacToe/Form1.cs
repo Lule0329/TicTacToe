2d8f8dbb33188a0c8164bc902f0299686e127704
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-03-12 15:40:03 +0100

    Gjorde så att vinn-systemet funkar, lade till så att spelet startar om efter nån har vunnit. Scores funkar fortfarande inte.

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -18,108 +18,89 @@ namespace TicTacToe
         }
 
         string turn = "X";
-        string Winner;
+        bool WinnerX = false;
+        bool WinnerO = false;
+        int XWins;
+        int OWins;
 
+        Image[] images = new Image[] { Properties.Resources.X, Properties.Resources.O, Properties.Resources.bg };
+
+        private void Restart()
+        {
+            pictureBox1.Image = images[2];
+            pictureBox2.Image = images[2];
+            pictureBox3.Image = images[2];
+            pictureBox4.Image = images[2];
+            pictureBox5.Image = images[2];
+            pictureBox6.Image = images[2];
+            pictureBox7.Image = images[2];
+            pictureBox8.Image = images[2];
+            pictureBox9.Image = images[2];
+
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            pictureBox3.Enabled = true;
+            pictureBox4.Enabled = true;
+            pictureBox5.Enabled = true;
+            pictureBox6.Enabled = true;
+            pictureBox7.Enabled = true;
+            pictureBox8.Enabled = true;
+            pictureBox9.Enabled = true;
+
+            turn = "X";
+            WinnerX = false;
+            WinnerO = false;
+
+            Xvinster.Text = "X vinster: " + XWins;
+            Ovinster.Text = "O vinster: " + OWins;
+        }
+        
         private void CheckWin()
         {
-            // Kollar om X vinner horizontalt
-            if (pictureBox1.Image == Properties.Resources.X && pictureBox2.Image == Properties.Resources.X && pictureBox3.Image == Properties.Resources.X)
+            // Kollar om O har vunnit horisontalt
+            if ((pictureBox1.Image == images[1] && pictureBox2.Image == images[1] && pictureBox3.Image == images[1]) ||
+        (pictureBox4.Image == images[1] && pictureBox5.Image == images[1] && pictureBox6.Image == images[1]) ||
+        (pictureBox7.Image == images[1] && pictureBox8.Image == images[1] && pictureBox9.Image == images[1]))
             {
-                Winner = "X";
-            }
-            else if (pictureBox4.Image == Properties.Resources.X && pictureBox5.Image == Properties.Resources.X && pictureBox6.Image == Properties.Resources.X)
-            {
-                Winner = "X";
-            }
-            else if (pictureBox7.Image == Properties.Resources.X && pictureBox8.Image == Properties.Resources.X && pictureBox9.Image == Properties.Resources.X)
-            {
-                Winner = "X";
-            }
-            else
-            {
-                Winner = " ";
+                WinnerO = true;
             }
 
-            // Kollar om O vinner horizontalt
-            if (pictureBox1.Image == Properties.Resources.O && pictureBox2.Image == Properties.Resources.O && pictureBox3.Image == Properties.Resources.O)
+            // Kollar om X har vunnit horisontalt
+            if ((pictureBox1.Image == images[0] && pictureBox2.Image == images[0] && pictureBox3.Image == images[0]) ||
+        (pictureBox4.Image == images[0] && pictureBox5.Image == images[0] && pictureBox6.Image == images[0]) ||
+        (pictureBox7.Image == images[0] && pictureBox8.Image == images[0] && pictureBox9.Image == images[0]))
             {
-                Winner = "O";
-            }
-            else if (pictureBox4.Image == Properties.Resources.O && pictureBox5.Image == Properties.Resources.O && pictureBox6.Image == Properties.Resources.O)
-            {
-                Winner = "O";
-            }
-            else if (pictureBox7.Image == Properties.Resources.O && pictureBox8.Image == Properties.Resources.O && pictureBox9.Image == Properties.Resources.O)
-            {
-                Winner = "O";
-            }
-            else
-            {
-                Winner = " ";
+                WinnerX = true;
             }
 
-            // Kollar om X vinner Vertikalt
-            if (pictureBox1.Image == Properties.Resources.X && pictureBox4.Image == Properties.Resources.X && pictureBox7.Image == Properties.Resources.X)
+            // Kollar om O har vunnit vertikalt
+            if ((pictureBox1.Image == images[1] && pictureBox4.Image == images[1] && pictureBox7.Image == images[1]) ||
+        (pictureBox2.Image == images[1] && pictureBox5.Image == images[1] && pictureBox8.Image == images[1]) ||
+        (pictureBox3.Image == images[1] && pictureBox6.Image == images[1] && pictureBox9.Image == images[1]))
             {
-                Winner = "X";
-            }
-            else if (pictureBox2.Image == Properties.Resources.X && pictureBox5.Image == Properties.Resources.X && pictureBox8.Image == Properties.Resources.X)
-            {
-                Winner = "X";
-            }
-            else if (pictureBox3.Image == Properties.Resources.X && pictureBox6.Image == Properties.Resources.X && pictureBox9.Image == Properties.Resources.X)
-            {
-                Winner = "X";
-            }
-            else
-            {
-                Winner = " ";
+                WinnerO = true;
             }
 
-            // Kollar om O vinner vertikalt
-            if (pictureBox1.Image == Properties.Resources.O && pictureBox4.Image == Properties.Resources.O && pictureBox7.Image == Properties.Resources.O)
+            // Kollar om X har vunnit vertikalt
+            if ((pictureBox1.Image == images[0] && pictureBox4.Image == images[0] && pictureBox7.Image == images[0]) ||
+        (pictureBox2.Image == images[0] && pictureBox5.Image == images[0] && pictureBox8.Image == images[0]) ||
+        (pictureBox3.Image == images[0] && pictureBox6.Image == images[0] && pictureBox9.Image == images[0]))
             {
-                Winner = "O";
-            }
-            else if (pictureBox2.Image == Properties.Resources.O && pictureBox5.Image == Properties.Resources.O && pictureBox8.Image == Properties.Resources.O)
-            {
-                Winner = "O";
-            }
-            else if (pictureBox3.Image == Properties.Resources.O && pictureBox6.Image == Properties.Resources.O && pictureBox9.Image == Properties.Resources.O)
-            {
-                Winner = "O";
-            }
-            else
-            {
-                Winner = " ";
+                WinnerX = true;
             }
 
-            // Kollar om X vinner diagonalt
-            if (pictureBox1.Image == Properties.Resources.X && pictureBox5.Image == Properties.Resources.X && pictureBox9.Image == Properties.Resources.X)
+            // Kollar om O har vunnit diagonalt
+            if ((pictureBox1.Image == images[1] && pictureBox5.Image == images[1] && pictureBox9.Image == images[1]) ||
+        (pictureBox3.Image == images[1] && pictureBox5.Image == images[1] && pictureBox7.Image == images[1]))
             {
-                Winner = "X";
-            }
-            else if (pictureBox3.Image == Properties.Resources.X && pictureBox5.Image == Properties.Resources.X && pictureBox7.Image == Properties.Resources.X)
-            {
-                Winner = "X";
-            }
-            else 
-            { 
-                Winner = " "; 
+                WinnerO = true;
             }
 
-            // Kollar om O vinner diagonalt
-            if (pictureBox1.Image == Properties.Resources.O && pictureBox5.Image == Properties.Resources.O && pictureBox9.Image == Properties.Resources.O)
+            // Kollar om X har vunnit diagonalt
+            if ((pictureBox1.Image == images[0] && pictureBox5.Image == images[0] && pictureBox9.Image == images[0]) ||
+        (pictureBox3.Image == images[0] && pictureBox5.Image == images[0] && pictureBox7.Image == images[0]))
             {
-                Winner = "O";
-            }
-            else if (pictureBox3.Image == Properties.Resources.O && pictureBox5.Image == Properties.Resources.O && pictureBox7.Image == Properties.Resources.O)
-            {
-                Winner = "O";
-            }
-            else
-            {
-                Winner = " ";
+                WinnerX = true;
             }
         }
 
@@ -133,26 +114,30 @@ namespace TicTacToe
             {
                 if (turn == "X")
                 {
-                    clickedPicturebox.Image = Properties.Resources.X;
+                    clickedPicturebox.Image = images[0];
                     turn = "O";
                     clickedPicturebox.Enabled = false;
                 }
                 else if (turn == "O")
                 {
-                    clickedPicturebox.Image = Properties.Resources.O;
+                    clickedPicturebox.Image = images[1];
                     turn = "X";
                     clickedPicturebox.Enabled = false;
                 }
             }
 
             CheckWin();
-            if (Winner == "X")
+            if (WinnerX == true)
             {
                 MessageBox.Show("X Wins");
+                XWins += 1;
+                Restart();
             }
-            else if (Winner == "O")
+            else if (WinnerO == true)
             {
                 MessageBox.Show("O Wins");
+                OWins += 1;
+                Restart();
             }
         }
     }
